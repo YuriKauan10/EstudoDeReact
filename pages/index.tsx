9421c86ceae1e26a13d5e2227125eef886c80667
Yuri007 <136291503+YuriKauan10@users.noreply.github.com>
2024-12-22 00:13:26 -0300

    novo

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Topo from "./Topo"
+import '../styles/globals.css';
+
+export default function Home() {
+  return (
+    <div>
+      <Topo/>
+      <div className="appDiv">
+        <div className="font-bold text-blue-700 text-">Curso de react NextTypescript</div>
+        <div className="bg-slate-400 text-orange-200 font-bold pl-1 pr-1 rounded-md">React</div>
+      </div>
+    </div>   
+  )
+}
