e53cb28d3e57337c048857365626f1adfc3a7a53
Yuri007 <136291503+YuriKauan10@users.noreply.github.com>
2024-12-22 10:59:10 -0300

    feat: renderização condicional, operador ternario

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Desabilita a regra para todo o projeto
+    },
+  },
 ];
+
+
 
 export default eslintConfig;
--- pages/Card.tsx
+++ pages/Card.tsx
@@ -10,8 +10,13 @@ function Card(props: CardProps){
         <div className="flex flex-col border border-red-700 rounded-sm p-2">
             <div className="cursor-pointer">Produto: {props.produto}</div>
             <div className="cursor-pointer">Valor : R${props.valor}</div>
-            <div>Desconto: R${props.desconto}</div>
-            <div>Preço Venda: R$ {props.funcao(props.valor,props.desconto)}</div>
+            {props.desconto>0?(
+                <div>
+                    <div>Desconto: R${props.desconto}</div>
+                    <div>Preço Venda: R$ {props.funcao(props.valor,props.desconto)}</div>
+                </div>
+            ):"Sem desconto"}
+
         </div>
     )
 }
