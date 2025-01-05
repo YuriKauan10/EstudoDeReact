f735a7d7f5547efc4d64eabe0b3820dd47993430
Yuri007 <136291503+YuriKauan10@users.noreply.github.com>
2025-01-05 10:28:03 -0300

    deixando mais enxuto as funções

--- pages/components/DisplayState.tsx
+++ pages/components/DisplayState.tsx
@@ -1,17 +1,19 @@
 interface DisplayStateProps{
     valor: number,
-    funcao: any,
-    funcao2: any
+    fvalor: any,
 }
 
 export default function DisplayState(props: DisplayStateProps){
+    function operacao(op:number){
+        props.fvalor(props.valor + op)
+    }
     return (
         <div>
             <div >
                 {props.valor}
             </div>
-             <button className="bg-zinc-700 text-white rounded-md p-1 m-1 hover:bg-zinc-800" onClick = {props.funcao}>adicionar</button>
-             <button className="bg-zinc-700 text-white rounded-md p-1 hover:bg-zinc-800" onClick = {props.funcao2}>subtrair</button>
+             <button className="bg-zinc-700 text-white rounded-md p-1 m-1 hover:bg-zinc-800" onClick = {() => operacao(1)}>adicionar</button>
+             <button className="bg-zinc-700 text-white rounded-md p-1 hover:bg-zinc-800" onClick = {() => operacao(-1)}>subtrair</button>
             </div>
     )
 }
--- pages/useState/usestate.tsx
+++ pages/useState/usestate.tsx
@@ -4,18 +4,10 @@ import DisplayState from "../components/DisplayState"
 export default function Usestate(){
     const [count, setCount] = useState<number>(0)
 
-    function adicionar(){
-        setCount(count + 1)
-    }
-
-    function subtrair(){
-        setCount(count - 1)
-    }
-
     return (
         <div>
             usestate
-            <DisplayState valor={count} funcao = {adicionar} funcao2 = {subtrair}></DisplayState>
+            <DisplayState valor={count} fvalor = {setCount}></DisplayState>
         </div>
     )
 }
