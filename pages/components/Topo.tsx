0b85740aefd2c22c9e24d0e8929f1a0a95ae53f2
Yuri007 <136291503+YuriKauan10@users.noreply.github.com>
2025-01-05 10:42:47 -0300

    usando inputs em react

--- pages/components/Topo.tsx
+++ pages/components/Topo.tsx
@@ -35,6 +35,9 @@ function Topo(){
             }}>
                 <p>useState</p>
             </Link>
+            <Link href= '/inputs/inputs'>
+                <p>Inputs</p>
+            </Link>
             </nav>
         </div>
     )
